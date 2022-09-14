c28f0e2a62df6560b52f1988a213d2ac28cd4f27
Lucas Parise <lucas.parise12@gmail.com>
2022-09-14 16:58:28 -0300

    entrega Clase 8

--- EntregaClase8/index.js
+++ EntregaClase8/index.js
@@ -1,9 +1,13 @@
 const fs = require("fs");
-
+const {routerProducto} = require("./routerProducto")
 const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 8080
 
 const server = app.listen(PORT,()=> {
-    console.log('servidor levantado')
+    console.log("servidor levantado en el puerto " + server.address().port);
 })
+
+app.use("/public", express.static("public"));
+
+app.use("/api/productos", routerProducto);
---
+++ EntregaClase8/routerProducto.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const routerProducto = express.Router();
+const {Contenedor} = require("./container");
+
+const contenedor = new Contenedor("productos.txt")
+
+/* ver todos los productos */
+routerProducto.get("/", async(req, res) => {
+    const listaProductos = await contenedor.getAll();
+    res.json(listaProductos);
+})
+
+
+/* ver producto por ID */
+routerProducto.get("/:id", async(req, res) => {
+    const productobuscado = await contenedor.getByID(req.params.id);
+    res.json(productobuscado ?? {error: "producto no encontrado"});
+})
+
+
+/* agregar producto */
+                                                /* a revisar */
+routerProducto.post("/", async (req, res) => {
+    let datos = req.body;
+    const producto = await contenedor.save(datos);
+    res.json(producto);
+})
+
+
+/* actualizar producto */
+routerProducto.put("/:id", async(req, res) => {
+    let datos = req.body
+    const producto = await contenedor.updateByID(datos, req.params.id);
+    res.json(producto);
+})
+
+
+/* borrar producto por ID */
+routerProducto.delete("/:id", async(req, res) => {
+    const resultado = await contenedor.deleteByID(req.params.id);
+    res.json(`Se elimino producto con el id: ${resultado}`);
+})
+
+
+
+module.exports = {routerProducto}
